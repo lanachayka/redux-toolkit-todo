5cd2f9d483f5c66fb8e87f3fb50a93e53ea10c91
Руслана Підсадюк <ruslana.chaikivska@gail.com>
2021-12-11 12:47:59 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [todos, setTodos] = useState([]);
+  const [text, setText] = useState('');
+
+  const addTodo = () => {
+    if (text.trim().length) {
+      setTodos([
+        ...todos,
+        {
+          id: new Date().toISOString(),
+          text: text,
+          completed: false
+        }
+      ]);
+      setText('');
+    }
+  }
+
+  const removeTodo = (id) => {
+    setTodos(todos.filter(todo => todo.id !== id))
+  }
+
+  const toogleTodoComplete = (id) => {
+    setTodos(
+      todos.map(todo => {
+        if (todo.id !== id) return todo;
+        return {
+          ...todo,
+          completed: !todo.completed
+        }
+      })
+    )
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todo List</h1>
+      <label>
+        <input value={text} onChange={(e) => setText(e.target.value)} />
+        <button onClick={addTodo}>Add Todo</button>
+      </label>
+      <ul>
+        {todos.map(todo => <li key={todo.id}>
+          <input type="checkbox" checked={todo.completed} onChange={()=>toogleTodoComplete(todo.id)}></input>
+          <span>{todo.text}</span>
+          <span className='delete' onClick={()=>removeTodo(todo.id)}>&times;</span>
+        </li>)}
+      </ul>
     </div>
   );
 }
